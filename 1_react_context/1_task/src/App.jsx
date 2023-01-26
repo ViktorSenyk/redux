7fdaed5bacbf8bf29cc108f3ff05b4178a990568
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-26 15:56:18 +0200

    + fix

--- 1_react_context/1_task/src/App.jsx
+++ 1_react_context/1_task/src/App.jsx
@@ -4,7 +4,7 @@ import { themes, ThemeContext } from './themes-context';
 
 export default class App extends Component {
   state = {
-    theme: themes.dark,
+    theme: themes.light,
   };
 
   toggleTheme = () =>
