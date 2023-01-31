ea33674a3980760a9fec08f292f2883a20392d09
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-31 17:42:44 +0200

    + fix

--- 2_intro_to_redux/3_task/src/index.js
+++ 2_intro_to_redux/3_task/src/index.js
@@ -1,11 +1,11 @@
 import store from "./store";
-import { ActionCreators } from './users.actions';
+import { addUser, deleteUser } from "./users.actions";
 
-store.dispatch(ActionCreators.add({ id: 50, name: 'Tom' }));
-store.dispatch(ActionCreators.add({ id: 5, name: 'Din' }));
-store.dispatch(ActionCreators.add({ id: 2, name: 'Bob' }));
+store.dispatch(addUser({ id: 50, name: 'Tom' }));
+store.dispatch(addUser({ id: 5, name: 'Din' }));
+store.dispatch(addUser({ id: 2, name: 'Bob' }));
 console.log(store.getState());
-store.dispatch(ActionCreators.del(5));
+store.dispatch(deleteUser(5));
 console.log(store.getState());
-store.dispatch(ActionCreators.del({ id: 80, name: 'AA' }));
+store.dispatch(deleteUser({ id: 80, name: 'AA' }));
 console.log(store.getState());
--- 2_intro_to_redux/3_task/src/users.actions.js
+++ 2_intro_to_redux/3_task/src/users.actions.js
@@ -1,7 +1,5 @@
 export const ADD = 'USERS/ADD';
 export const DEL = 'USERS/DEL';
 
-export const ActionCreators = {
-    add: newUser => ({ type: ADD, value: newUser }),
-    del: id => ({ type: DEL, value: id })
-}
+export const addUser = newUser => ({ type: ADD, value: newUser });
+export const deleteUser = id => ({ type: DEL, value: id });
