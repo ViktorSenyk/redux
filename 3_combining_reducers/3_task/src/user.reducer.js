76cab184a683fa11cc119f29f0653db634922703
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-02-01 00:06:37 +0200

    + fix

--- 3_combining_reducers/3_task/src/user.reducer.js
+++ 3_combining_reducers/3_task/src/user.reducer.js
@@ -3,9 +3,7 @@ import { USER_SET, USER_REMOVE } from './user.actions';
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case USER_SET:
-      return {
-        name: action.payload.user,
-      };
+      return action.payload.user;
     case USER_REMOVE:
       return null;
     default:
