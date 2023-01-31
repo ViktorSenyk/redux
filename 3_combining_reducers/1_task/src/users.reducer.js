3aab76f84906278c46bdd2150e2f49e568d8b609
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-31 19:59:13 +0200

    + fix

--- 3_combining_reducers/1_task/src/users.reducer.js
+++ 3_combining_reducers/1_task/src/users.reducer.js
@@ -2,30 +2,24 @@ import { ADD, DELETE, UPDATE } from './users.actions';
 
 const usersList = [];
 
-const usersReducer = (state = { users: { usersList } }, action) => {
+const usersReducer = (state = { usersList }, action) => {
   switch (action.type) {
     case ADD:
       return {
         ...state,
-        users: { ...state.users, usersList: [...state.users.usersList, action.payload.newUser] },
+        usersList: [...state.usersList, action.payload.newUser],
       };
     case DELETE:
       return {
         ...state,
-        users: {
-          ...state.users,
-          usersList: state.users.usersList.filter(user => user.id !== action.payload.userId),
-        },
+        usersList: state.usersList.filter(user => user.id !== action.payload.userId),
       };
     case UPDATE:
       return {
         ...state,
-        users: {
-          ...state.users,
-          usersList: state.users.usersList.map(user =>
-            user.id === action.payload.userId ? { ...user, ...action.payload.newUserInfo } : user,
-          ),
-        },
+        usersList: state.usersList.map(user =>
+          user.id === action.payload.userId ? { ...user, ...action.payload.newUserInfo } : user,
+        ),
       };
     default:
       return state;
