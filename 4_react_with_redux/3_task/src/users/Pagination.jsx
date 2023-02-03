36706d4a72da62ddc549f46e48b350df73ff728b
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-02-03 20:32:05 +0200

    + fix

--- 4_react_with_redux/3_task/src/users/Pagination.jsx
+++ 4_react_with_redux/3_task/src/users/Pagination.jsx
@@ -4,19 +4,17 @@ export default class Pagination extends Component {
   render = () => {
     const { currentPage, goNext, goPrev, totalItems } = this.props;
     const lastPageNumber = Math.ceil(totalItems / 3);
+    const isPrevPageAvailable = currentPage === 0;
+    const isNextPageAvailable = currentPage + 1 === lastPageNumber;
 
     return (
       <div className="pagination">
-        <button className="btn" onClick={() => goPrev()} disabled={currentPage === 0}>
-          {currentPage !== 0 && '←'}
+        <button className="btn" onClick={() => goPrev()} disabled={isPrevPageAvailable}>
+          {!isPrevPageAvailable && '←'}
         </button>
         <span className="pagination__page">{currentPage + 1}</span>
-        <button
-          className="btn"
-          onClick={() => goNext()}
-          disabled={currentPage + 1 === lastPageNumber}
-        >
-          {currentPage + 1 !== lastPageNumber && '→'}
+        <button className="btn" onClick={() => goNext()} disabled={isNextPageAvailable}>
+          {!isNextPageAvailable && '→'}
         </button>
       </div>
     );
