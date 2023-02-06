c8a43309acc1cb0fbfb0991a69738a8228b7e277
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-02-06 16:50:28 +0200

    add second way

--- 5_selectors/1_task/src/store.js
+++ 5_selectors/1_task/src/store.js
@@ -1,19 +1,19 @@
-import { createStore, combineReducers } from 'redux';
-import { usersReducer } from './users/users.reducer';
-
-const appReducer = combineReducers({ users: usersReducer });
-
-const store = createStore(appReducer);
-
-export default store;
-
 // import { createStore, combineReducers } from 'redux';
-// import usersReducer from './users/users.reducer';
+// import { usersReducer } from './users/users.reducer';
 
-// const reducer = combineReducers({
-//   users: usersReducer,
-// });
+// const appReducer = combineReducers({ users: usersReducer });
 
-// const store = createStore(reducer);
+// const store = createStore(appReducer);
 
 // export default store;
+
+import { createStore, combineReducers } from 'redux';
+import usersReducer from './users/users.reducer';
+
+const reducer = combineReducers({
+  users: usersReducer,
+});
+
+const store = createStore(reducer);
+
+export default store;
